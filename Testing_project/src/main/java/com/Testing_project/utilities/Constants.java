8964a8334b410e0e424fa02fe258293163a5f3cf
calinmarchis <calinmarchis@EVO249.evointern>
2014-10-20 15:23:13 +0300

    constantele

---
+++ Testing_project/src/main/java/com/Testing_project/utilities/Constants.java
@@ -0,0 +1,9 @@
+package com.Testing_project.utilities;
+
+public class Constants {
+
+	public static final String USERNAME = "MCM";
+	public static final String URL = "http://192.168.1.68:9090/home";
+	public static final String Password = "Ev@zon01";
+	
+}
