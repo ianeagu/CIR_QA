f1d4a63edae39c12ac55501aa658076728ec21fb
ramonatoadere <ramonatoadere@EVZ666.evointern>
2014-10-21 12:07:51 +0300

    New Vacation Request Submenu

--- Testing_project/src/test/java/com/Testing_project/steps/NewVacationRequestSteps.java
+++ Testing_project/src/test/java/com/Testing_project/steps/NewVacationRequestSteps.java
@@ -1,10 +1,12 @@
 package com.Testing_project.steps;
 
+import java.text.ParseException;
+
 import com.Testing_project.pages.NewVacationRequestPage;
+
 import net.thucydides.core.annotations.Step;
 import net.thucydides.core.pages.Pages;
 import net.thucydides.core.steps.ScenarioSteps;
-
 import static ch.lambdaj.Lambda.join;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
@@ -26,5 +28,48 @@ public class NewVacationRequestSteps extends ScenarioSteps {
 	{
 		newVacationRequestPage.verifyThatYouAreOnCreateNewVacationRequestPage();
 	}
+	
+	
+	
+	
+	@Step
+	public void selectStartDate(int month, int day, int year) throws ParseException
+	{
+		newVacationRequestPage.clickStartDateBox();
+		newVacationRequestPage.setDate(month, day, year);
+		
+	}
+
+	public void selectEndDate(int month, int day, int year) throws ParseException {
+		newVacationRequestPage.clickEndDateBox();
+		newVacationRequestPage.setDate(month, day, year);
+		
+	}
+	
+	@Step
+	public void clickNewVacationSickLeaveBtn()
+	{
+		newVacationRequestPage.clickNewVacationSickLeaveBtn();
+	}
+	
+	@Step
+	public void clickNewVacationSaveBtn()
+	{
+		newVacationRequestPage.clickNewVacationSaveBtn();
+	}
+	
+//	@Step
+//	public void clickNewVacationBackBtn()
+//	{
+//		newVacationRequestPage.clickNewVacationBackBtn();
+//	}
+//	
+//	@Step
+//	public void verifyThatYouAreOnMyRequestsPage()
+//	{
+//	newVacationRequestPage.verifyThatYouAreOnMyRequestsPage();
+//		
+//	}
+	
 	}
 	
