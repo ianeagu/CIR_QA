cb29602e74a0e92243933417c1c9afa260a668b1
calinmarchis <calinmarchis@EVO249.evointern>
2014-10-21 12:06:09 +0300

    Vacation page

--- Testing_project/src/test/java/com/Testing_project/VacationTest.java
+++ Testing_project/src/test/java/com/Testing_project/VacationTest.java
@@ -1,17 +1,18 @@
 package com.Testing_project;
 
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.ManagedPages;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.pages.Pages;
+import net.thucydides.junit.runners.ThucydidesRunner;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
 import com.Testing_project.steps.EndUserSteps;
 import com.Testing_project.steps.HomeSteps;
-
-import net.thucydides.core.annotations.Managed;
-import net.thucydides.core.annotations.ManagedPages;
-import net.thucydides.core.annotations.Steps;
-import net.thucydides.core.pages.Pages;
-import net.thucydides.junit.runners.ThucydidesRunner;
+import com.Testing_project.steps.VacationSteps;
 
 @RunWith(ThucydidesRunner.class)
 public class VacationTest {
@@ -24,12 +25,15 @@ public class VacationTest {
 	public EndUserSteps endUser;
 	@Steps
 	public HomeSteps homeSteps;
+	@Steps
+	VacationSteps vacationSteps;
 
 	@Test
 	public void test_logIn() {
 		endUser.is_the_home_page();
 		endUser.login();
 		homeSteps.clickVacationTab();
+		vacationSteps.verifyThatYouAreOnVacationPage("Vacation");
 	}
 
 	
--- Testing_project/src/test/java/com/Testing_project/steps/HomeSteps.java
+++ Testing_project/src/test/java/com/Testing_project/steps/HomeSteps.java
@@ -1,11 +1,15 @@
 package com.Testing_project.steps;
 
-import com.Testing_project.pages.HomePage;
-
 import net.thucydides.core.annotations.Step;
 import net.thucydides.core.steps.ScenarioSteps;
 
+import com.Testing_project.pages.HomePage;
+
 public class HomeSteps extends ScenarioSteps {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	HomePage homePage;
 
 	@Step
