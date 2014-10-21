dd9a56cff2cd7f10cb451f65736d84376d4e6c95
irinaneagu <irinaneagu@EVO365.evointern>
2014-10-21 17:29:29 +0300

    I added tests for "My requests" page

--- src/main/java/com/Testing_project/pages/MyRequestsPage.java
+++ src/main/java/com/Testing_project/pages/MyRequestsPage.java
@@ -1,26 +1,50 @@
 package com.Testing_project.pages;
 
-import java.util.List;
-
+import net.thucydides.core.annotations.findby.FindBy;
 import net.thucydides.core.pages.PageObject;
-
-import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import net.thucydides.core.pages.WebElementFacade;
 
 public class MyRequestsPage extends PageObject {
-	public void verifyThatYouAreOnVacationPage(String navigationHeaderTitle) {
-		List<WebElement> navHeaders = getDriver().findElements(
-				By.cssSelector("div.content-title"));
-		boolean found = false;
-		for (WebElement navHeader : navHeaders) {
-			if (navHeader.getText().toLowerCase()
-					.equals(navigationHeaderTitle)) {
-				System.out.println(navHeader.getText().toLowerCase());
-				found = true;
-				break;
-			}
-			Assert.assertTrue("You are not on vacation page!", found);
-		}
+	
+	//identificare buton - "New Request"
+	@FindBy(css = "div[class*='carousel'] a[href*='menuItem=my-request']")
+	public WebElementFacade myRequestsButton;
+	
+	//identificare text - "Show only future vacations"
+	@FindBy(css = "label[for*='futureVacationsCheckbox']")
+	public WebElementFacade futureVacationsLabel;
+	
+	//identificare check-box "holiday" din "Vacation Type"
+	@FindBy(css = "label[for*='HOLIDAYCheckbox']")
+	public WebElementFacade holidayVacationType;
+	
+	//identificare buton "Apply"
+	@FindBy(css = "input[id='_evovacation_WAR_EvoVacationportlet_applyButton']")
+	public WebElementFacade applyButton;
+	
+	//identifiare prim element - selectie "Vacation Type" - holiday
+	@FindBy(css = "td[id='_evovacation_WAR_EvoVacationportlet_evozonVacationsSearchContainer_col-my.request.column.header.type_row-1']")
+	public WebElementFacade firstFieldTypeColumn;
+	
+	//metoda - click buton "New Request"
+	public void clickOnMyRequests() {
+		myRequestsButton.click();
 	}
+	
+	//metoda - verificare prezenta element in pagina
+	public boolean labelIsPresent() {
+		return futureVacationsLabel.isPresent();
+	}
+		
+	//metoda - click checkbox "Holiday"
+	public void clickOnHoliday() {
+		holidayVacationType.click();
+	}
+	
+	//metoda - click buton "Apply"
+	public void clickOnApplyButton() {
+		applyButton.click();
+	}
+		
 }
+
